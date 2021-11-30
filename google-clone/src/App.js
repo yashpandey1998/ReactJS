0030f01012c4e3a789fe9887a17eef7480a5d956
ayush pandey <ayushpandey8439@gmail.com>
2021-11-30 21:10:46 +0100

    Fix link and react router

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import {BrowserRouter as Router , Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
-      <Home/>
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home/>}>
+                </Route>
+            </Routes>
+        </Router>
     </div>
   );
 }
--- google-clone/src/pages/Home.js
+++ google-clone/src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Home.css';
+import {Link} from 'react-router-dom';
+
 
 export default function Home() {
     return (
         <div>
             <h1>This is the Home Page in google clone!!</h1>
+
+            <div>
+                <Link to='/about'> About</Link>
+            </div>
         </div>
     );
 }
