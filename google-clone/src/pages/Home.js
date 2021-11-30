1979de27d748a4be59349b68d7bdc173d6a8e83e
ayush pandey <ayushpandey8439@gmail.com>
2021-11-30 20:50:09 +0100

    Fix issues with build

--- google-clone/src/pages/Home.js
+++ google-clone/src/pages/Home.js
@@ -1,33 +1,10 @@
 import React from 'react';
 import './Home.css';
-import { Link } from "react-router-dom";
 
-function Home () {
+export default function Home() {
     return (
-        <div className='home'>
-            <h1>This is home page</h1>
-
-            <div className='home_header'>
-                <div className='home_headerLeft'>
-                    <Link to ='about'>About</Link>
-                    {/*Link*/}
-                </div>
-                <div className='home_headerRight'>
-                    {/*Link*/}
-                    {/*Link*/}
-                    {/*Icor*/}
-                    {/*Avatar*/}   
-
-                </div>
-
-            </div>
-
-            <div className='home_body'>
-
-            </div>
-
+        <div>
+            <h1>This is the Home Page in google clone!!</h1>
         </div>
-    )
+    );
 }
-
-export default Home  
